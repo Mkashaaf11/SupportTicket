23ae2744f69e05647e9f7b23d0182819b9fe5dca
Mkashaaf11 <muhammadkashaf331@gmail.com>
2025-06-19 17:42:30 +0500

    Initial commit of minimal api project

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/api/status", () =>
+{
+    return Results.Ok(new
+    {
+        status = "Working",
+        time = DateTime.Now,
+    });
+});
 
 app.Run();
