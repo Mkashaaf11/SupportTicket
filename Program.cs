7628ed8820c2629b20c728b570ded2d916a440be
Mkashaaf11 <muhammadkashaf331@gmail.com>
2025-06-20 13:18:36 +0500

    Add Ticket model and /api/tickets endpoint

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using SupportTicket.Models;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
@@ -8,6 +9,17 @@ app.MapGet("/api/status", () =>
         status = "Working",
         time = DateTime.Now,
     });
+});
+
+var tickets = new List<Ticket>
+{
+    new Ticket{Id = 1,Title = "Bug Ticket" , Description = "It is s ticket for bug"},
+    new Ticket {Id=2 , Title = "Login Issue", Description = "Unable to login", Status = "In Progress"}
+};
+
+app.MapGet("api/getTickets", () => { 
+
+return Results.Ok(tickets);
 });
 
 app.Run();
